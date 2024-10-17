9e7a784cb07007054584e6cf325c88a59ce78e35
Skilly <badanin060@gmail.com>
2024-10-17 21:25:51 +0300

    add base

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello world... oh sorry... hello AniBu)')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
